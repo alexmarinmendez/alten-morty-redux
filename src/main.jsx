c5b3d2c6d73901721dbd4abaed360585e058a50d
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-27 14:53:59 -0500

    feat: initial redux setup

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import { compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import reducer from './reducers'
+
+const initialState = {
+  favoriteCharacters: [],
+  sectionActive: "Characters"
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(logger)))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
