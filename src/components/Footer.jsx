69bbf76df7be6044fdc3ef68c59b01b5e2ff704e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-28 12:43:57 -0500

    feat: implements footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import "../assets/styles/components/Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="footer__made">
+        <div>
+          Made with 💓 by <a href="http://www.alten.com" target="_blank">ALTEN GROUP</a>
+        </div>
+      </div>
+      <div className="footer__location">
+        SPAIN, 2023
+      </div>
+    </footer>
   )
 }
 
